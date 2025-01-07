d9a997f8284fceec1d91aacb26e125b64020f452
Gergely Risko <errge@nilcons.com>
2025-01-08 00:53:09 +0100

    Add contents with jupytext

--- jupyter/02-build.sh
+++ jupyter/02-build.sh
@@ -3,5 +3,9 @@
 set -xeuo pipefail
 
 . .venv/bin/activate
-rm -rf .jupyterlite.doit.db _output
-jupyter lite build --no-sourcemaps
+rm -rf .jupyterlite.doit.db _output contents-ipynb
+mkdir -p contents-ipynb
+for f in contents/*.py; do
+    jupytext --to ipynb -o contents-ipynb/$(basename $f .py).ipynb $f
+done
+jupyter lite build --no-sourcemaps --contents contents-ipynb
