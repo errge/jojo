013d5206d2af0ad458f92b245ba2a3d7239e1c01
Gergely Risko <errge@nilcons.com>
2025-01-15 22:40:10 +0100

    Add throttler to the bicycle widget example

--- jupyter/contents/20250115-eve-joe-bicycle.py
+++ jupyter/contents/20250115-eve-joe-bicycle.py
@@ -10,6 +10,50 @@
 # When and where will they be closest to each other?
 
 # %% jupyter={"source_hidden": true}
+# below code is copy-paste from here: https://ipywidgets.readthedocs.io/en/8.1.5/examples/Widget%20Events.html#throttling
+import asyncio
+from time import time
+
+class Timer:
+    def __init__(self, timeout, callback):
+        self._timeout = timeout
+        self._callback = callback
+
+    async def _job(self):
+        await asyncio.sleep(self._timeout)
+        self._callback()
+
+    def start(self):
+        self._task = asyncio.ensure_future(self._job())
+
+    def cancel(self):
+        self._task.cancel()
+
+def throttle(wait):
+    """ Decorator that prevents a function from being called
+        more than once every wait period. """
+    def decorator(fn):
+        time_of_last_call = 0
+        scheduled, timer = False, None
+        new_args, new_kwargs = None, None
+        def throttled(*args, **kwargs):
+            nonlocal new_args, new_kwargs, time_of_last_call, scheduled, timer
+            def call_it():
+                nonlocal new_args, new_kwargs, time_of_last_call, scheduled, timer
+                time_of_last_call = time()
+                fn(*new_args, **new_kwargs)
+                scheduled = False
+            time_since_last_call = time() - time_of_last_call
+            new_args, new_kwargs = args, kwargs
+            if not scheduled:
+                scheduled = True
+                new_wait = max(0, wait - time_since_last_call)
+                timer = Timer(new_wait, call_it)
+                timer.start()
+        return throttled
+    return decorator
+# copy-paste ends here, below is original content
+
 from ipycanvas import MultiCanvas
 from math import sqrt
 from ipywidgets import FloatSlider, interact, Layout
@@ -42,6 +86,7 @@ def reset_canvas():
 def bicycle_position(time, startingpos, velocity):
     return startingpos + velocity * time
 
+@throttle(0.2)
 def draw(time):
     reset_canvas()
 
