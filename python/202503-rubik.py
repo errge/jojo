19f2219009f725f8b7280a7e4135f788547fd863
Gergely Risko <errge@nilcons.com>
2025-02-12 03:06:31 +0100

    Way better UI
    1. Let the terminal do the zooming
    2. In exchange we can draw pretty blocks
    3. Add animations
    4. Center the game horizontally and vertically
    5. Turn off terminal autowrap
    6. Draw black on whole screen, so no garbage after resize

--- python/202503-rubik.py
+++ python/202503-rubik.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
 
 from copy import deepcopy
+from math import ceil
+import os
 from termcolor import colored
 import sys
 import termios
 import time
 import tty
 
+def pr(str):
+    print(str, end = '')
+
+# useful terminal operations
+# see http://www.braun-home.net/michael/info/misc/VT100_commands.htm
+def cursorhome():
+    pr(chr(27) + '[H')
+
+def nextline():
+    # A very safe next line: go one line down and 1000 characters to the left
+    # But all these movement commands NEVER scroll, so no flickering on the screen
+    pr(chr(27) + '[1B' + chr(27) + '[1000D')
+
+colors = {
+    'W': 'white',
+    'G': 'green',
+    'O': 'yellow',
+    'Y': 'light_yellow',
+    'R': 'red',
+    'B': 'blue',
+    ' ': 'light_grey',
+}
+
 def colorchar(char, color):
-    if color == ord('W'):
-        return colored(char, 'white')
-    if color == ord('G'):
-        return colored(char, 'green')
-    if color == ord('O'):
-        return colored(char, 'yellow')
-    if color == ord('Y'):
-        return colored(char, 'light_yellow')
-    if color == ord('R'):
-        return colored(char, 'red')
-    if color == ord('B'):
-        return colored(char, 'blue')
-    if color == ord(' '):
-        return colored(char, 'light_grey')
-    raise Exception(f'wtf {char} {color}')
+    pr(colored(char, colors[chr(color)], 'on_light_grey'))
 
 upside    = [(0, 3), (0, 4), (0, 5), (1, 5), (2, 5), (2,4), (2, 3), (1, 3)]
 uprow     = [(3, x) for x in range(12)]
@@ -35,19 +46,10 @@ rightcol  = [(y, 5) for y in range(9)] + [(5, 9), (4, 9), (3, 9)]
 leftside  = [(y, x-6) for (y, x) in rightside]
 leftcol   = [(y, 3) for y in range(9)] + [(5, 11), (4, 11), (3, 11)]
 
-def rotatestate(state, how, howmuch):
-    newstate = deepcopy(state)
-    for i in range(len(how)):
-        y, x = how[i]
-        y_, x_ = how[(i+howmuch) % len(how)]
-        newstate[y][x] = state[y_][x_]
-    return newstate
-
 class Cube:
     def __init__(self):
-        # m for multiplier, mx and my is just for zooming
-        self.mx = 4
         self.my = 2
+        self.anim = 0.05
         self.state = []
         for i in range(3):
             self.state.append(bytearray(' ' * 3 + 'W' * 3 + ' ' * 6, encoding = 'ascii'))
@@ -57,55 +59,92 @@ class Cube:
             self.state.append(bytearray(' ' * 3 + 'Y' * 3 + ' ' * 6, encoding = 'ascii'))
 
     def draw(self):
-        print(chr(27) + "[H", end = '')
-        print()
-        print()
-        print()
-        print(colored('                ' + '█' * (self.mx * 12 + 2), 'light_grey'))
+        w, h = os.get_terminal_size()
+        dw, dh = 63, 20
+        vpad = int((h - dh) / 2)
+        hpad = ' ' * ceil((w - dw) / 2)
+        cursorhome()
+        for _ in range(vpad):
+            pr(' ' * w)
+            nextline()
+        pr(colored(hpad + '█' * dw + hpad, 'light_grey'))
+        nextline()
         for r in self.state:
-            for _ in range(self.my):
-                print(colored('                █', 'light_grey'), end = '')
+            for repeat in range(2):
+                pr(colored(hpad + '██', 'light_grey'))
                 for c in r:
-                    print(colorchar('█', c) * self.mx, end = '')
-                print(colored('█', 'light_grey'))
-        print(colored('                ' + '█' * (self.mx * 12 + 2), 'light_grey'))
+                    if repeat == 0:
+                        colorchar('▇▇▇▇ ', c)
+                    if repeat == 1:
+                        colorchar('🮆🮆🮆🮆 ', c)
+                pr(colored('█', 'light_grey') + hpad)
+                nextline()
 
-    def up(self):
-        newstate = rotatestate(self.state, upside, -2)
-        newstate = rotatestate(newstate, uprow, 3)
+        # pr(hpad + colored(f'{f"Height: {h}, Width: {w} ":>{dw}}', 'black', 'on_light_grey') + hpad)
+        pr(hpad + colored(f'{f"Anim speed: {self.anim:.2f}s ":>{dw}}', 'black', 'on_light_grey') + hpad)
+        for _ in range(vpad + 1):
+            nextline()
+            pr(' ' * w)
+        sys.stdout.flush()
+
+    def rotatestate(self, how, howmuch, anim = True):
+        newstate = deepcopy(self.state)
+        for i in range(len(how)):
+            y, x = how[i]
+            y_, x_ = how[(i+howmuch) % len(how)]
+            newstate[y][x] = self.state[y_][x_]
         self.state = newstate
+        if anim:
+            self.draw()
+            time.sleep(self.anim)
 
-    def down(self):
-        newstate = rotatestate(self.state, downrow, -3)
-        newstate = rotatestate(newstate, downside, -2)
-        self.state = newstate
+    def up(self, mul = 1):
+        self.rotatestate(uprow, 1 * mul)
+        for i in range(2):
+            self.rotatestate(uprow, 1 * mul, False)
+            self.rotatestate(upside, -1 * mul)
 
-    def right(self):
-        newstate = rotatestate(self.state, rightcol, 3)
-        newstate = rotatestate(newstate, rightside, -2)
-        self.state = newstate
+    def down(self, mul = 1):
+        self.rotatestate(downrow, -1 * mul)
+        for i in range(2):
+            self.rotatestate(downrow, -1 * mul, False)
+            self.rotatestate(downside, -1 * mul)
 
-    def left(self):
-        newstate = rotatestate(self.state, leftcol, -3)
-        newstate = rotatestate(newstate, leftside, -2)
-        self.state = newstate
+    def right(self, mul = 1):
+        self.rotatestate(rightcol, 1 * mul)
+        for i in range(2):
+            self.rotatestate(rightcol, 1 * mul, False)
+            self.rotatestate(rightside, -1 * mul)
 
-    def cuberight(self):
-        self.up()
-        self.up()
-        self.up()
-        self.down()
-        self.state = rotatestate(self.state, midrow, -3)
+    def left(self, mul = 1):
+        self.rotatestate(leftcol, 1 * mul)
+        for i in range(2):
+            self.rotatestate(leftcol, 1 * mul, False)
+            self.rotatestate(leftside, 1 * mul)
 
-    def cubeup(self):
-        self.right()
-        self.left()
-        self.left()
-        self.left()
-        self.state = rotatestate(self.state, midcol, 3)
+    def cuberight(self, mul = 1):
+        for i in range(3):
+            if i: self.rotatestate(upside, 1 * mul, False)
+            if i: self.rotatestate(downside, -1 * mul, False)
+            self.rotatestate(uprow, -1 * mul, False)
+            self.rotatestate(midrow, -1 * mul, False)
+            self.rotatestate(downrow, -1 * mul)
 
+    def cubeup(self, mul = 1):
+        for i in range(3):
+            if i: self.rotatestate(leftside, 1 * mul, False)
+            if i: self.rotatestate(rightside, -1 * mul, False)
+            self.rotatestate(leftcol, 1 * mul, False)
+            self.rotatestate(midcol, 1 * mul, False)
+            self.rotatestate(rightcol, 1 * mul)
+
+# cbreak mode means, that we read characters from the terminal wo waiting for newline
 tty_attrs = tty.setcbreak(1)
-print(chr(27) + "[2J", end = '')
+# clear screen
+pr(chr(27) + '[2J')
+# and we turn autowrap off, as we hand position the cursor and
+pr(chr(27) + '[?7l')
+
 try:
     cube = Cube()
     undo = []
@@ -118,48 +157,41 @@ try:
                 if len(undo) >= 2:
                     undo.pop()
                     cube.state = undo.pop()
-            case '1':
-                cube.mx += 1
-            case '2':
-                cube.my += 1
             case 'Q':
                 break
             case 'u':
                 cube.up()
             case 'i':
-                cube.up()
-                cube.up()
-                cube.up()
+                cube.up(-1)
             case 'k':
                 cube.down()
             case 'j':
-                cube.down()
-                cube.down()
-                cube.down()
+                cube.down(-1)
             case 'o':
                 cube.right()
             case 'l':
-                cube.right()
-                cube.right()
-                cube.right()
+                cube.right(-1)
             case 'y':
                 cube.left()
-                cube.left()
-                cube.left()
             case 'h':
-                cube.left()
+                cube.left(-1)
             case 'd':
                 cube.cuberight()
             case 'a':
-                cube.cuberight()
-                cube.cuberight()
-                cube.cuberight()
+                cube.cuberight(-1)
             case 'w':
                 cube.cubeup()
             case 's':
-                cube.cubeup()
-                cube.cubeup()
-                cube.cubeup()
+                cube.cubeup(-1)
+            case '+':
+                cube.anim += 0.01
+            case '-':
+                cube.anim -= 0.01
+                cube.anim = max(cube.anim, 0)
 
 finally:
+    # restore input buffering
     termios.tcsetattr(1, termios.TCSAFLUSH, tty_attrs)
+    # wrap back on
+    pr(chr(27) + '[?7h')
+    sys.stdout.flush()
