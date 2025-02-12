632f13df13871b98fcf61c6ccb9e5af0fe59875b
Gergely Risko <errge@nilcons.com>
2025-02-12 12:11:33 +0100

    front, back and shuffle

--- python/202503-rubik.py
+++ python/202503-rubik.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 from copy import deepcopy
+from itertools import product
 from math import ceil
 import os
-from termcolor import colored
+from random import choice
 import sys
+from termcolor import colored
 import termios
 import time
 import tty
@@ -12,14 +14,23 @@ import tty
 def pr(str):
     print(str, end = '')
 
-# useful terminal operations
-# see http://www.braun-home.net/michael/info/misc/VT100_commands.htm
+# Good VT100 description: http://www.braun-home.net/michael/info/misc/VT100_commands.htm
 def cursorhome():
     pr(chr(27) + '[H')
 
+def clearscreen():
+    pr(chr(27) + '[2J')
+
+def wrapoff():
+    pr(chr(27) + '[?7l')
+
+def wrapon():
+    pr(chr(27) + '[?7h')
+
 def nextline():
-    # A very safe next line: go one line down and 1000 characters to the left
-    # But all these movement commands NEVER scroll, so no flickering on the screen
+    # A very safe next line: go one line down and 1000 characters to the left.
+    # But all these movement commands NEVER scroll (not even on the last line).
+    # So we can accidentally overrun, and still no flickering of the screen.
     pr(chr(27) + '[1B' + chr(27) + '[1000D')
 
 colors = {
@@ -40,11 +51,15 @@ uprow     = [(3, x) for x in range(12)]
 midrow    = [(4, x) for x in range(12)]
 midcol    = [(y, 4) for y in range(9)] + [(5, 10), (4, 10), (3, 10)]
 downrow   = [(5, x) for x in range(12)]
-downside  = [(y+6, x) for (y, x) in upside]
+downside  = [(y + 6, x) for (y, x) in upside]
 rightside = [(3, 6), (3, 7), (3, 8), (4, 8), (5, 8), (5, 7), (5, 6), (4, 6)]
 rightcol  = [(y, 5) for y in range(9)] + [(5, 9), (4, 9), (3, 9)]
-leftside  = [(y, x-6) for (y, x) in rightside]
+leftside  = [(y, x - 6) for (y, x) in rightside]
 leftcol   = [(y, 3) for y in range(9)] + [(5, 11), (4, 11), (3, 11)]
+frontside = [(3, 3), (3, 4), (3, 5), (4, 5), (5, 5), (5, 4), (5, 3), (4, 3)]
+frontcirc = [(2, 3), (2, 4), (2, 5), (3, 6), (4, 6), (5, 6), (6, 5), (6, 4), (6, 3), (5, 2), (4, 2), (3, 2)]
+backside  = [(y, x + 6) for (y, x) in frontside]
+backcirc  = [(0, 3), (0, 4), (0, 5), (3, 8), (4, 8), (5, 8), (8, 5), (8, 4), (8, 3), (5, 0), (4, 0), (3, 0)]
 
 class Cube:
     def __init__(self):
@@ -122,6 +137,18 @@ class Cube:
             self.rotatestate(leftcol, 1 * mul, False)
             self.rotatestate(leftside, 1 * mul)
 
+    def front(self, mul = 1):
+        self.rotatestate(frontcirc, 1 * mul)
+        for i in range(2):
+            self.rotatestate(frontcirc, 1 * mul, False)
+            self.rotatestate(frontside, 1 * mul)
+
+    def back(self, mul = 1):
+        self.rotatestate(backcirc, 1 * mul)
+        for i in range(2):
+            self.rotatestate(backcirc, 1 * mul, False)
+            self.rotatestate(backside, -1 * mul)
+
     def cuberight(self, mul = 1):
         for i in range(3):
             if i: self.rotatestate(upside, 1 * mul, False)
@@ -140,18 +167,20 @@ class Cube:
 
 # cbreak mode means, that we read characters from the terminal wo waiting for newline
 tty_attrs = tty.setcbreak(1)
-# clear screen
-pr(chr(27) + '[2J')
-# and we turn autowrap off, as we hand position the cursor and
-pr(chr(27) + '[?7l')
+clearscreen()
+wrapoff()
 
+todo = []
 try:
     cube = Cube()
     undo = []
     while True:
         cube.draw()
         undo.append(cube.state)
-        key = sys.stdin.read(1)
+        if todo:
+            key = todo.pop(0)
+        else:
+            key = sys.stdin.read(1)
         match key:
             case 'z':
                 if len(undo) >= 2:
@@ -175,6 +204,14 @@ try:
                 cube.left()
             case 'h':
                 cube.left(-1)
+            case 'n':
+                cube.front()
+            case 'm':
+                cube.front(-1)
+            case '7':
+                cube.back()
+            case '8':
+                cube.back(-1)
             case 'd':
                 cube.cuberight()
             case 'a':
@@ -188,10 +225,25 @@ try:
             case '-':
                 cube.anim -= 0.01
                 cube.anim = max(cube.anim, 0)
+            case 'N':
+                shuffle = list(product(['L', 'F', 'U', 'D', 'B'], [-1, 1, 2]))
+                for s in range(20):
+                    match choice(shuffle):
+                        case ('L', i):
+                            cube.left(i)
+                        case ('R', i):
+                            cube.right(i)
+                        case ('F', i):
+                            cube.front(i)
+                        case ('B', i):
+                            cube.back(i)
+                        case ('U', i):
+                            cube.up(i)
+                        case ('D', i):
+                            cube.down(i)
 
 finally:
     # restore input buffering
     termios.tcsetattr(1, termios.TCSAFLUSH, tty_attrs)
-    # wrap back on
-    pr(chr(27) + '[?7h')
+    wrapon()
     sys.stdout.flush()
